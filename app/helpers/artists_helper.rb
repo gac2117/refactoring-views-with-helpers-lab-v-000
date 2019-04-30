cde969df311b310531520d4e6f366375a05986d2
Grace Choi <oiograceoio@gmail.com>
2019-04-30 18:08:19 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     if song.artist
       link_to song.artist, artist_path(song.artist)
     else
-      redirect_to edit_song_path(song)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
